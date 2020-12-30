495f8227eb713660dde2263b1bacf23a2bce2d4d
Christopher Ray <vde435@gmail.com>
2020-12-30 00:17:59 -0600

    Create vector_size2.cpp

---
+++ vector_size2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+ 
+int main() {
+  
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+    
+  // Add more 
+  
+  grocery.push_back("Artichoke");
+  grocery.push_back("Eggs");
+  
+  std::cout << grocery.size() << "\n";
+  
+}
